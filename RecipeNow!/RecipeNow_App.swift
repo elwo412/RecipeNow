f94fcdad227437e971c86ab8f6898064cec68a8d
Thomas Worrall <elliottworrall@gmail.com>
2023-06-27 15:03:51 -0400

    Initial Commit

---
+++ RecipeNow!/RecipeNow_App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeNow_App.swift
+//  RecipeNow!
+//
+//  Created by Thomas Worrall on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeNow_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
